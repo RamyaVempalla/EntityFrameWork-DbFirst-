7c2c8c517597eb7a9ada3728baf7ee7a832f73f2
Ramya Vempalla <Ramya.Vempalla@neudesic.com>
2018-07-21 16:27:56 +0530

    Entity framework DbFirst

---
+++ EmployeeEntityFrame/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Neudesic.EmployeeEntityFrame.DataContext;
+using Neudesic.EmployeeEntityFrame.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Neudesic.EmployeeEntityFrame.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Employee")]
+    public class EmployeeController : Controller
+    {
+            [HttpGet]
+            public ActionResult Get()
+            {
+                using (var EmpContext = new EmployeeDataContext())
+                {
+                    var EmployeesList = new List<EmployeeModel>();
+                    EmployeesList = EmpContext.EmployeesDetails.ToList();
+                    return Ok(EmployeesList);
+                }
+            }
+
+            [HttpPost]
+            public ActionResult Post([FromBody]EmployeeModel EmployeeObject)
+            {
+                using (var EmpContext = new EmployeeDataContext())
+                {
+                    EmpContext.EmployeesDetails.Add(EmployeeObject);
+                    EmpContext.SaveChanges();
+                }
+                return Ok("Query processed");
+            }
+
+            [HttpPut]
+            public ActionResult Put([FromBody]EmployeeModel EmployeeObject)
+            {
+                using (var EmpContext = new EmployeeDataContext())
+                {
+                    EmpContext.EmployeesDetails.Update(EmployeeObject);
+                    EmpContext.SaveChanges();
+                }
+                return Ok("Query processed");
+            }
+
+            [HttpDelete]
+            public ActionResult Delete(EmployeeModel EmployeeObject)
+            {
+                using (var EmpContext = new EmployeeDataContext())
+                {
+                    EmpContext.EmployeesDetails.Remove(EmployeeObject);
+                    EmpContext.SaveChanges();
+                }
+                return Ok("Query processed");
+            }
+    }
+}
---
+++ EmployeeEntityFrame/DataContext/EmployeeDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Neudesic.EmployeeEntityFrame.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neudesic.EmployeeEntityFrame.DataContext
+{
+    public class EmployeeDataContext : DbContext
+    {
+        public DbSet<EmployeeModel> EmployeesDetails { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; Database=Employee; Trusted_Connection=True; MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
